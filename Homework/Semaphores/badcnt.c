bc13945f33f627fc3406e4f4a2640ab77a74f3fc
nyanji00 <wl77886@gmail.com>
2023-05-28 10:58:12 +0900

    badcnt

---
+++ Homework/Semaphores/badcnt.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+// #include <sys/time.h>
+
+volatile int cnt = 0;
+
+void *thread(void *vargp)
+{
+  int i, niters = *((int *)vargp);
+
+  for (i = 0; i < niters; i++)
+    cnt++;
+  
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  int niters = atoi(argv[1]);
+  pthread_t tid1, tid2;
+
+  // struct timeval start_time, end_time;
+  // long elapsed_time;
+
+  // gettimeofday(&start_time, NULL);
+
+  pthread_create(&tid1, NULL, thread, &niters);
+  pthread_create(&tid2, NULL, thread, &niters);
+  pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
+
+  // gettimeofday(&end_time, NULL);
+
+  // elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000000L + (end_time.tv_usec - start_time.tv_usec);
+
+  /* Check result */
+  if (cnt != (2 * niters))
+    printf("BOOM! cnt=%d\n", cnt);
+  else
+    printf("OK cnt=%d\n", cnt);
+
+  // printf("Elapsed time: %ld microseconds\n", elapsed_time);
+
+  exit(0);
+}
